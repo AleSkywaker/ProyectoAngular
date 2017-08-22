fee25dbef4ec1d8d7c32f2206d46938269b12c34
Alex Colombo <uricaine@hotmail.com>
2017-08-22 12:14:38 +0200

    tokken

--- controllers/user.js
+++ controllers/user.js
@@ -84,8 +84,26 @@ function saveUser(req, res){
         })
     }
 
+    function updateUser(req, res){
+        var userId = req.params.id;
+        var update = req.body;
+
+        User.findByIdAndUpdate(userId, update, (err, userUpdated)=>{
+            if(err){
+                res.status(500).send({mensaje: 'Error al actualizar el usuario'})
+            }else{
+                if(!userUpdated){
+                    res.status(404).send({mensaje: 'No se ha podido actualizar el usuario'})
+                }else{
+                    res.status(200).send({user: userUpdated})
+                }
+            }
+        });
+    }
+
 module.exports = {
     pruebas,
     saveUser,
-    loginUser
+    loginUser,
+    updateUser
 }
