d8dd76223051ba9eb6b6dfd0693bde1287d1cebc
Alex Colombo <uricaine@hotmail.com>
2017-08-22 09:18:17 +0200

    creando Api

--- controllers/user.js
+++ controllers/user.js
@@ -21,7 +21,7 @@ function saveUser(req, res){
     user.name = params.name;
     user.surname = params.surname;
     user.email = params.email;
-    user.role = 'ROLE_USER';
+    user.role = 'ROLE_ADMIN';
     user.image = 'null';
 
     if(params.password){
@@ -30,20 +30,58 @@ function saveUser(req, res){
             user.password = hash;
                 if(user.name != null && user.surname && user.email != null){
                     //Guardar usuario
+                    user.save((err, userStored)=>{
+                        if(err){
+                            res.status(500).send({mensaje:"Error al guardar el usuario"})
+                        }else{
+                            if(!userStored){
+                                res.status(400).send({mensaje: 'No se ha registrado el usuario'})
+                            }else{
+                                res.status(200).send({user:userStored})
+                            }
+                        }
+                    })
                 }else{
                     res.status(200).send({mensaje: 'Rellena todos los campos'})
                 }
         });
-
-
-
     }else{
         res.status(500).send({mensaje: 'introduce la contraseña'})
     }
-
 }
+    function loginUser(req, res){
+        var params = req.body;
+
+        var email = params.email;
+        var password = params.password;
+
+        User.findOne({email: email.toLowerCase()}, (err, user)=>{
+            if(err){
+                res.status(500).send({mensaje: "Error en la peticion"})
+            }else{
+                if(!user){
+                    res.status(404).send({mensaje: 'El usuario no existe'})
+                }else{
+                    //comparar contraseña
+                    bcrypt.compare(password, user.password, (err, check)=>{
+                        if(check){
+                            //devolver los datos del usario logeado
+                            if(params.gethash){
+                                //devolver un token de jwt
+                            }else{
+                                res.status(200).send({user});
+                            }
+                        }else{
+                            res.status(404).send({mensaje: 'El usuario no ha podido logearse'})
+                        }
+                    })
+                }
+            }
+        })
+    }
 
 module.exports = {
     pruebas,
-    saveUser
+    saveUser,
+    loginUser
 }
---
+++ services/jwt.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var jwt = require('jwt-simple');
+var moment = require('moment');
+
+
+exports.createToken = function(user){
+   
+      
+         
+};
