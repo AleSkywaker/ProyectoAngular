adfc6816477f1181d611a1a37ca9b7a70936b40d
Alex Colombo <uricaine@hotmail.com>
2017-08-23 09:10:27 +0200

    delete method

--- controllers/artist.js
+++ controllers/artist.js
@@ -53,11 +53,13 @@ function getArtists(req, res){
   
   var itemsPerPage = 3;
 
-  if(req.params.page){
+  /* if(req.params.page){
     var page = req.params.page;
   }else{
     var page = 1;
-  }
+  } */
+
+  var page = req.params.page || 1
 
   Artist.find().sort('name').paginate(page, itemsPerPage, function(err, artists, totalItems){
     if(err){
@@ -67,7 +69,7 @@ function getArtists(req, res){
             res.status(404).send({mensaje:'No hay artistas'})
         }else{
             return res.status(200).send({
-                pages:totalItems,
+                total_items:totalItems,
                 artists:artists
             })
         }
@@ -75,8 +77,67 @@ function getArtists(req, res){
   })
 }
 
+function updateArtist(req, res){
+      var artistId = req.params.id;
+      var update = req.body;
+
+      Artist.findByIdAndUpdate(artistId, update, (err, artistUpdated)=>{
+        if(err){
+            res.status(500).send({mensaje: 'Error al guardar artista'})
+        }else{
+            if(!artistUpdated){
+                res.status(404).send({mensaje: 'No se ha podido guardar artista'})
+            }else{
+                res.status(200).send({artist: artistUpdated})
+            }
+        }
+      })
+}
+
+function deleteArtist(req, res){
+    var artistId = req.params.id;
+
+    Artist.findByIdAndRemove(artistId, (err, artistRemoved)=>{
+        if(err){
+            res.status(500).send({mensaje: 'Error al eliminar artista'})
+        }else{
+            if(!artistRemoved){
+                res.status(404).send({mensaje: 'El artista no pudo ser eliminado'})
+            }else{
+                
+
+                Album.find({artist: artistRemoved._id}).remove((err, albumRemoved)=>{
+                    if(err){
+                        res.status(500).send({mensaje: 'Error al eliminar album'})
+                    }else{
+                        if(!albumRemoved){
+                            res.status(404).send({mensaje: 'El album no pudo ser eliminado'})
+                        }else{
+
+
+                            Song.find({album: albumRemoved._id}).remove((err, songRemoved)=>{
+                                if(err){
+                                    res.status(500).send({mensaje: 'Error al eliminar una cancion'})
+                                }else{
+                                    if(!songRemoved){
+                                        res.status(404).send({mensaje: 'la cancion no pudo ser eliminado'})
+                                    }else{
+                                        res.status(200).send({artist: artistRemoved})
+                                    }
+                                }
+                            })
+                        }
+                    }
+                })
+            }
+        }
+    })
+}
+
 module.exports = {
     getArtist,
     saveArtist,
-    getArtists
+    getArtists,
+    updateArtist,
+    deleteArtist
 }
