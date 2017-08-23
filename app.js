d959dca51330aa19fbf742d4375b9c0ab1ef3ef4
Alex Colombo <uricaine@hotmail.com>
2017-08-23 14:14:53 +0200

    song upload

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var app = express();
 var user_routes = require('./routes/user');
 var artist_routes = require('./routes/artist');
 var album_routes = require('./routes/album');
+var song_routes = require('./routes/song');
+
 
 
 app.use(bodyParser.urlencoded({extended:false}));
@@ -23,6 +25,9 @@ app.use(bodyParser.json());
 app.use('/api', user_routes);
 app.use('/api', artist_routes);
 app.use('/api', album_routes);
+app.use('/api', song_routes);
+
+
 
 // exportar module
 
